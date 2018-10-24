3e6da273908d7fd7519d4e8a03141781b626d81d
Alessandro Maglioccola <alessandro@maglioccola.com>
2018-10-24 21:38:20 +0200

    Enabled Cross Origin requests

--- src/main/java/gigroup/exercises/sudokugenerator/controller/SudokuGeneratorController.java
+++ src/main/java/gigroup/exercises/sudokugenerator/controller/SudokuGeneratorController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import gigroup.exercises.sudokugenerator.service.SudokuGeneratorService;
 
@@ -15,6 +16,7 @@ public class SudokuGeneratorController {
 	private SudokuGeneratorService sudokuGenerator;
 
 	@GetMapping(path = "/generate")
+	@CrossOrigin(origins = "http://localhost:3000")
 	public int[][] getSudokuTable() {
 		return sudokuGenerator.generate();
 	}
