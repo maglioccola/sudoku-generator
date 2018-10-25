a3e25bb8e7329c92cce10fbe2586303bcba67f9a
Alessandro Maglioccola <alessandro.maglioccola@sorint.it>
2018-10-25 19:29:44 +0200

    Method checkValue

--- src/main/java/gigroup/exercises/sudokugenerator/controller/SudokuGeneratorController.java
+++ src/main/java/gigroup/exercises/sudokugenerator/controller/SudokuGeneratorController.java
@@ -1,11 +1,14 @@
 package gigroup.exercises.sudokugenerator.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
+import gigroup.exercises.sudokugenerator.model.Params;
 import gigroup.exercises.sudokugenerator.service.SudokuGeneratorService;
 
 @RestController
@@ -19,6 +22,12 @@ public class SudokuGeneratorController {
 	@CrossOrigin(origins = "http://localhost:3000")
 	public int[][] getSudokuTable() {
 		return sudokuGenerator.generate();
+	}
+
+	@PostMapping(path = "/check")
+	@CrossOrigin(origins = "http://localhost:3000")
+	public boolean check(@RequestBody final Params params) {
+		return sudokuGenerator.checkValue(params.getMatrix(), params.getRow(), params.getCol(), params.getNum());
 	}
 
 }
